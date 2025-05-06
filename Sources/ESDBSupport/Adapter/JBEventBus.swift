6b8be4c3b84f30c48b36a3b3f6ba52ffb7ae35d4
Grady Ghuo <gradyzhuo@gmail.com>
2025-05-07 01:03:10 +0800

    [UPDATE] JBEventBus handle RecordedEvent

---
+++ Sources/ESDBSupport/Adapter/JBEventBus.swift
@@ -0,0 +1,25 @@
+//
+//  JBEventBus.swift
+//  DDDKit
+//
+//  Created by Grady Zhuo on 2025/4/23.
+//
+import DDDCore
+import JBEventBus
+import KurrentDB
+
+extension JBEventBus{
+    
+    private func publish<Subscriber: EventSubscriber>(of subscriber: Subscriber, event: RecordedEvent) async throws{
+        guard let event = try event.decode(to: Subscriber.Event.self) else {
+            return
+        }
+        try await subscriber.handle(event)
+    }
+    
+    public func postEvent(event: RecordedEvent) async throws {
+        for subscriber in self.eventSubscribers {
+            try await publish(of: subscriber, event: event)
+        }
+    }
+}
