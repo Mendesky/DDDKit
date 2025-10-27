b2361bc3c2d61f4e417ada7317f1db80e78d7066
Grady Zhuo <gradyzhuo@jwcpas.com.tw>
2025-10-27 23:26:37 +0800

    [UPDATE] update PresenterOutput.

--- Sources/DDDCore/Adapter/Presenter/PresenterOutput.swift
+++ Sources/DDDCore/Adapter/Presenter/PresenterOutput.swift
@@ -1,11 +1,7 @@
 import Foundation
 
-public struct PresenterOutput<ReadModelType> {
-    public let readModel: ReadModelType
-    public let message: String?
-    
-    internal init(readModel: ReadModelType, message: String?) {
-        self.readModel = readModel
-        self.message = message
-    }
+public protocol PresenterOutput {
+    associatedtype ReadModelType: ReadModel
+    var readModel: ReadModelType { get }
+    var message: String? { get }
 }
